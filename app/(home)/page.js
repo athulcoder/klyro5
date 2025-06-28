df650330582141d123ead4bf49644d45aa7bc860
athulcoder <athul.sin.cos.tan@gmail.com>
2025-06-28 13:32:08 +0530

    service tab completed

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -7,9 +7,9 @@ export default function HomePage() {
   return (
     <div className="relative w-full h-screen">
       <GradientBackground />
-      <div className="relative z-20 flex flex-col h-full p-3 text-white text-3xl font-bold bg-[#141414]">
-        <h1>Our Services</h1>
-        <div className="flex overflow-scroll gap-4 w-full overflow-x-auto whitespace-nowrap scrollbar-hidden my-4">
+      <div className="relative z-20 flex flex-col h-full p-3 text-white text-3xl font-bold bg-[#141414] min-lg:px-6">
+        <h1 className="">Our Services</h1>
+        <div className="flex overflow-scroll gap-4 w-full  overflow-x-auto whitespace-nowrap scrollbar-hidden my-4 min-lg:flex min-lg:gap-6 ">
           {ServiceList.map((item) => {
             return (
               <ServiceCard
--- components/ServiceCard.js
+++ components/ServiceCard.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function ServiceCard({ title, image, description }) {
   return (
-    <div className="bg-[#2d2d2d] min-w-[200px]   shadow-xl h-[300px] rounded-lg  transition  duration-300 ">
+    <div className="bg-[#2d2d2d] min-w-[200px] max-w-[300px]  min-lg:w-[240px] shadow-xl h-[310px] min-lg:h-[340px] rounded-lg  transition  duration-300 ">
       <div className="w-full h-[180px] relative">
         <Image
           src={image}
@@ -13,9 +13,11 @@ function ServiceCard({ title, image, description }) {
           className="rounded-t-lg"
         />
       </div>
-      <div className="p-2 flex flex-col gap-2">
-        <h1 className="text-xs font-semibold text-white text-wrap">{title}</h1>
-        <p className="text-gray-400 text-[12px] text-left text-wrap">
+      <div className="p-2 flex flex-col gap-2 ">
+        <h1 className="text-xs font-semibold text-white text-wrap min-lg:text-sm">
+          {title}
+        </h1>
+        <p className="text-gray-400 text-[12px] text-left text-wrap min-lg:text-[13px]">
           {description}
         </p>
       </div>
