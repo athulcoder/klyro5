c642f230aaab41e1eeff8d63e73ddd3667186132
athulcoder <athul.sin.cos.tan@gmail.com>
2025-07-03 14:06:01 +0530

    client model

--- models/client.model.js
+++ models/client.model.js
@@ -1,3 +1,13 @@
 import mongoose from "mongoose";
 
-const clientSchema = new mongoose.Schema({}, { timestamps: true });
+const clientSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, lowercase: true },
+    phone: { type: stringify, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Client =
+  mongoose.models.Client || mongoose.model("Client", clientSchema);
