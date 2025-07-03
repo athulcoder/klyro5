d0d71a91bf7536519444add62f768b300b9efbef
athulcoder <athul.sin.cos.tan@gmail.com>
2025-07-03 08:55:42 +0530

    db connection made

--- app/api/verify/route.js
+++ app/api/verify/route.js
@@ -1,3 +1,4 @@
+import { connectDB } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
@@ -9,4 +10,8 @@ export async function POST(req) {
       message: "All Fields are required ",
     });
   }
+
+  try {
+    await connectDB();
+  } catch (error) {}
 }
