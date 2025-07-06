9107a4ceb53a2a10723cbfd36b18b0adb01b319d
athulcoder <athul.sin.cos.tan@gmail.com>
2025-07-06 14:02:52 +0530

    model for project

--- models/project.model.js
+++ models/project.model.js
@@ -1,6 +1,33 @@
 import mongoose from "mongoose";
 
-const projectSchema = new mongoose.Schema({}, { timestamps: true });
+const projectSchema = new mongoose.Schema(
+  {
+    client: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Client",
+    },
+    companyName: { type: String, required: true },
+    companyEmail: { type: String, required: true, lowercase: true },
+    companyAdress: { type: String, required: true },
+    companyDescription: { type: String, required: true },
+
+    // Socail midea links
+    instagramLink: { type: String, required: true },
+    githubLink: { type: String },
+    facebookLink: { type: String },
+    xappLink: { type: String },
+    linkedinLink: { type: String },
+
+    //Project
+    domainName: { type: String, lowercase: true },
+    pagesRequired: { type: [String], required: true },
+    websiteStyle: { type: String, required: true },
+
+    // Media file
+    fileUrls: { type: [String] },
+  },
+  { timestamps: true }
+);
 
 export const Project =
   mongoose.models.Project || mongoose.model("Project", projectSchema);
