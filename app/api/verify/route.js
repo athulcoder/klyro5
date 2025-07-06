a95ec64de332f015a3fd2e01d51c292ae188d7b6
athulcoder <athul.sin.cos.tan@gmail.com>
2025-07-06 14:53:24 +0530

    api for form submission

--- app/api/verify/route.js
+++ app/api/verify/route.js
@@ -4,15 +4,39 @@ import { Client } from "@/models/client.model";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  const { fullName, phone, email } = await req.json();
+  const formData = await req.json();
 
-  if (!fullName || !phone || !email) {
+  console.log(formData.fullName, formData.email, formData.phone);
+  if (!formData.fullName || !formData.phone || !formData.email) {
     return NextResponse.json({
       success: false,
       message: "All fields are required",
     });
   }
-  const res = verifyClient({ fullName, phone, email });
 
-  return res;
+  // first we create the client and then we add the project with the client id that is the ObjectId
+
+  const res = await verifyClient(
+    formData.fullName,
+    formData.phone,
+    formData.email
+  );
+
+  if (
+    !formData.companyName ||
+    !formData.companyDescription ||
+    !formData.companyEmail ||
+    !formData.companyAddress ||
+    !formData.instagramLink ||
+    !formData.pagesRequired
+  ) {
+    return NextResponse.json({
+      succes: false,
+      messaage: "Some fields are requried",
+    });
+  }
+
+  console.log(formData);
+  if (res)
+    return NextResponse.json({ succes: true, message: "Client Created" });
 }
