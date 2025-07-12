f2e78e2773de4bbc3dfc66f142bcdb9f612faeed
athulcoder <athul.sin.cos.tan@gmail.com>
2025-07-12 19:55:12 +0530

    gradient bg changed

--- components/GradientBackground.js
+++ components/GradientBackground.js
@@ -9,7 +9,7 @@ export default function GradientBackground() {
     <div className="relative w-full h-screen overflow-hidden bg-[#0b1120]">
       {/* Energy blobs */}
       <div className="absolute w-full h-full overflow-hidden z-0">
-        <div className="absolute w-96 h-96 bg-pink-800 opacity-30 blur-3xl rounded-full animate-pulse-slow top-1/4 left-1/3" />
+        <div className="absolute w-96 h-96 bg-red-800 opacity-30 blur-3xl rounded-full animate-pulse-slow top-1/4 left-1/3" />
         <div className="absolute w-80 h-80 bg-blue-800 opacity-20 blur-2xl rounded-full animate-pulse-fast top-2/3 left-1/4" />
         <div className="absolute w-64 h-64 bg-yellow-500 opacity-25 blur-2xl rounded-full animate-pulse-mid top-1/2 left-2/3" />
       </div>
