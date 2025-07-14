404d76a2a3198b98b6982d1d1123d887bc8a9104
athulcoder <athul.sin.cos.tan@gmail.com>
2025-07-14 17:06:27 +0530

    message

--- components/FormSubmitMessage.js
+++ components/FormSubmitMessage.js
@@ -4,7 +4,7 @@ import { CheckCircle } from "lucide-react";
 function FormSubmitMessage() {
   return (
     <div className="flex flex-col items-center justify-center p-6 bg-gray-900 rounded-2xl shadow-md max-w-md mx-auto mt-10 text-white">
-      <CheckCircle className="text-green-500 w-12 h-12 mb-3" />
+      <CheckCircle className="text-green-600 w-12 h-12 mb-3" />
       <h2 className="text-xl font-semibold mb-1">Thank you!</h2>
       <p className="text-center text-gray-300 text-sm">
         Your form has been submitted successfully. <br />
